ddabf0cf307a2459145a48a3a3af51efccd20d08
Farrij-Rifai <ripaiparrij@gmail.com>
2020-10-06 21:25:25 +0700

    Menambahkan Login login.php pada view

---
+++ application/views/login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Login Admin</title>
+  <link rel="stylesheet" href="<?= base_url('assets/templat') ?>/css/bootstrap.min.css" />
+
+  <link rel="stylesheet" href="<?= base_url('assets/templat') ?>/css/style.css" />
+</head>
+ <body>
+    <div class="container">
+      <div class="row justify-content-center mt-5">
+        <div class="col-md-4">
+          <div class="card">
+            <div class="card-header bg-transparent mb-0"><h5 class="text-center">Please <span class="font-weight-bold text-primary">LOGIN</span></h5></div>
+            <div class="card-body">
+              <form action="">
+                <div class="form-group">
+                  <input type="text" name="" class="form-control" placeholder="Username">
+                </div>
+                <div class="form-group">
+                  <input type="text" name="" class="form-control" placeholder="Password">
+                </div>
+                <div class="form-group custom-control custom-checkbox">
+                  <input type="checkbox" class="custom-control-input" id="customControlAutosizing">
+                  <label class="custom-control-label" for="customControlAutosizing">Remember me</label>
+                </div>
+                <div class="form-group">
+                  <input type="submit" name="" value="Login" class="btn btn-primary btn-block">
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+
+</html>
